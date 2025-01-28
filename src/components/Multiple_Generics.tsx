488aa79db63b31e1a225e3e66f4a902a2da0ab75
Yogesh Rana <yr70416@gmail.com>
2025-01-28 16:59:27 +0530

    Update Multiple_Generics.tsx
    Adding the Comment

--- src/components/Multiple_Generics.tsx
+++ src/components/Multiple_Generics.tsx
@@ -1,4 +1,6 @@
 //Example of Function Multiple Generices in TypeScript
+// T will be inferred as string (the type of "Test")
+// U will be inferred as number (the type of 1)
 
 function ToRecord<TypeOfValue,TypeOfId>(id : TypeOfId, value: TypeOfValue) {
 // function ToRecord< T, U >(id : TypeOfId, value: TypeOfValue)  -------> Use the shortform on above code
